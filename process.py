e750a0c4ac68edf9085e2c1cb517e106c5af1ba2
johnwashburne <johnwashburne@outlook.com>
2019-10-16 13:27:13 -0400

    Add files via upload

--- process.py
+++ process.py
@@ -7,5 +7,20 @@ with open("sudoku.csv") as csvfile:
     for row in readCSV:
         if row[1] != 'solutions':
             games.append(row[1])
+
+
+
+with open("sudoku.csv") as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    games_short = []
+    count = 0
+    for row in readCSV:
+        if count > 2000:
+            break
+        elif row[1] != 'solutions':
+            games_short.append(row[1])
+            count += 1
+
 pickle.dump(games, open("games.p", "wb"))
+pickle.dump(games_short, open("games_short.p", "wb"))
 
